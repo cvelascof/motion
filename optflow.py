f3b7326c66898cd184e41df8d59a57e31cb57b8d
Seppo Pulkkinen <pulkkins@gmail.com>
2018-06-07 16:04:05 +0200

    Add optflow.py

---
+++ optflow.py
@@ -0,0 +1,31 @@
+"""Interface and utility methods for different optical flow algorithms."""
+
+# TODO: Implement this method.
+def get_method(name):
+  """Return a callable function for the optical flow method corresponding to 
+  the given name. The available options are:\n\
+  
+  +----------------------------------------------------------------------------+
+  | Python-based implementations                                               |
+  +-------------------+--------------------------------------------------------+
+  |     Name          |              Description                               |
+  +===================+========================================================+
+  |  lucaskanade      | OpenCV implementation of the Lucas-Kanade method       |
+  |                   | with interpolated motion vectors for areas with no     |
+  |                   | precipitation (by Daniele Nerini)                      |
+  +-------------------+--------------------------------------------------------+
+  
+  +----------------------------------------------------------------------------+
+  | Methods implemented in C (these require separate compilation and linkage)  |
+  +-------------------+--------------------------------------------------------+
+  |     Name          |              Description                               |
+  +===================+========================================================+
+  |  brox             | implementation of the variational method of Brox et al.|
+  |                   | (2004) from IPOL (http://www.ipol.im/pub/art/2013/21)  |
+  +-------------------+--------------------------------------------------------+
+  |  clg              | implementation of the Combined Local-Global (CLG)      |
+  |                   | method of Bruhn et al., 2005 from IPOL                 |
+  |                   | (http://www.ipol.im/pub/art/2015/44)                   |
+  +-------------------+--------------------------------------------------------+
+  """
+  pass
